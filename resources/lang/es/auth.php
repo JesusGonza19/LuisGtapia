729a1f78e21c9207690e923dd011352f345b6e80
Jesus <al221610469@gmail.com>
2018-10-04 01:15:14 -0500

    04/10/2018, se cambio la informacion de este archivo por las intrucciones en español

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'throttle' => 'Demaciados interntos de acceso. Porfavor intente de nuevo en :seconds segundos.',
 
 ];
+
