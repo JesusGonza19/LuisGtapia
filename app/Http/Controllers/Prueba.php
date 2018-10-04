edccc2be3f9dc9550d68389b2c9275aa2296f26d
Jesus <al221610469@gmail.com>
2018-10-04 03:34:52 -0500

    04/10/2018, Primera prueba para controladores

---
+++ app/Http/Controllers/Prueba.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Luisgtapia\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Luisgtapia\Http\Requests;
+
+class Prueba extends Controller{
+
+//___________________________________________________________________________________________________________________
+
+//Prueba controladores - esto va de la mano con una ruta
+
+    public function index()
+{
+    return "Hola desde controller";
+}
+
+//__________________________________________________________________________________________________________________
+
+}
+
+
+
